e617fb0becf3cb9ead32d49e50f502af0975c61b
qbgaoo <qbgaoo@126.com>
2024-10-22 13:40:05 +0800

    commit

--- example-numbering.lua
+++ example-numbering.lua
@@ -3,7 +3,7 @@ local counter = 0
 function Div(el)
   if el.classes:includes('example') then
     counter = counter + 1
-    -- 为 example 类的 Div 添加编号，并用 <strong> 包裹
+    -- Add a number to the Div of the example class and wrap it with <strong>
     table.insert(
       el.content, 1, 
       pandoc.RawInline('html', '<strong>Example ' .. counter .. ':</strong> '))
