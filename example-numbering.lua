9a75db95d62094613ac22424aa83b4d515080f8b
qbgaoo <qbgaoo@126.com>
2024-11-19 10:02:01 +0800

    commit

--- example-numbering.lua
+++ example-numbering.lua
@@ -6,7 +6,7 @@ function Div(el)
     -- Add a number to the Div of the example class and wrap it with <strong>
     table.insert(
       el.content, 1, 
-      pandoc.RawInline('html', '<strong>Example ' .. counter .. ':</strong> '))
+      pandoc.RawInline('html', '<strong>Example ' .. counter .. '</strong> '))
     return el
   end
 end
